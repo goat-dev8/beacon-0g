4c2464c54d4a8b35dbb8b556dedb39fef40d7668
goat-dev8 <goat-dev8@users.noreply.github.com>
2026-08-25 09:32:08 +0300

    feat: add index

---
+++ packages/receipts/src/index.ts
@@ -0,0 +1,12 @@
+export {
+  buildReceipt,
+  validateReceipt,
+} from "./build.js";
+export type {
+  BeaconReceipt,
+  BuildReceiptInput,
+  ReceiptAcceptBinding,
+  ReceiptArtifactRef,
+  ReceiptOfferBinding,
+  ReceiptPaymentBinding,
+} from "./build.js";
